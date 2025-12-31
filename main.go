500455750b678018d715e12134553f1c1919eada
iamBelugaa <iamnilotpaldeka@gmail.com>
2025-12-31 21:40:53 +0530

    add new bufio.Scanner implementation, test and benchmarking test

--- main.go
+++ main.go
@@ -23,21 +23,37 @@ func main() {
 		log.Fatalln("failed to read current working dir :", err)
 	}
 
-	file, err := os.Open(filepath.Join(wd, "utf-8.txt"))
+	file, err := os.Open(filepath.Join(wd, "words.txt"))
 	if err != nil {
 		log.Fatalln("failed to open file :", err)
 	}
+
 	defer func() {
 		if err := file.Close(); err != nil {
 			log.Fatalln("failed to close file :", err)
 		}
 	}()
 
-	wordCount := CountWords(file)
+	wordCount := CountWordsUsingBufioScanner(file)
 	fmt.Println("Word Count =>", wordCount)
 }
 
-func CountWords(reader io.Reader) int {
+// CountWordsUsingBufioScanner counts words using a buffered scanner.
+func CountWordsUsingBufioScanner(reader io.Reader) int {
+	var wordCount int
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		_ = scanner.Text()
+		wordCount++
+	}
+
+	return wordCount
+}
+
+// CountWordsUsingBufioReader counts words using bufio.Reader and checking for whitespace.
+func CountWordsUsingBufioReader(reader io.Reader) int {
 	var wordCount int
 	var isInsideWord bool
 	bufReader := bufio.NewReaderSize(reader, bufferSize)
@@ -48,19 +64,23 @@ func CountWords(reader io.Reader) int {
 			break
 		}
 
+		// If the rune is not a space and we're not inside a word, it's the start of a new word
 		if !unicode.IsSpace(r) && !isInsideWord {
 			wordCount++
 		}
+
 		isInsideWord = !unicode.IsSpace(r)
 	}
 
 	return wordCount
 }
 
+// CustomCountWords is a custom implementation for counting words.
 func CustomCountWords(reader io.Reader) int {
 	var wordCount int
 	var isInsideWord bool
 
+	// Buffers for reading input and storing leftover bytes.
 	leftover := make([]byte, 0)
 	buf := make([]byte, bufferSize)
 
@@ -70,6 +90,7 @@ func CustomCountWords(reader io.Reader) int {
 			break
 		}
 
+		// Append any leftover bytes from previous reads to the current buffer.
 		subbuf := append(leftover, buf[:size]...)
 
 		for len(subbuf) > 0 {
@@ -78,6 +99,7 @@ func CustomCountWords(reader io.Reader) int {
 				break
 			}
 
+			// If the rune is not whitespace and we're not inside a word, it's a new word.
 			subbuf = subbuf[rsize:]
 			if !unicode.IsSpace(r) && !isInsideWord {
 				wordCount++
@@ -86,6 +108,7 @@ func CustomCountWords(reader io.Reader) int {
 			isInsideWord = !unicode.IsSpace(r)
 		}
 
+		// Store any leftover bytes that didn't form a complete rune.
 		leftover = leftover[:0]
 		leftover = append(leftover, subbuf...)
 	}
